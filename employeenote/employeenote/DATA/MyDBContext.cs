e4f6c65b142f286f2dc1d61c71b4b27917be1fd1
goenka2 <shubhamgoenka2@gmail.com>
2023-08-14 09:41:00 +0530

    hii

---
+++ employeenote/employeenote/DATA/MyDBContext.cs
@@ -0,0 +1,21 @@
+﻿using employeenote.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace employeenote.DATA
+{
+    public class MyDBContext:DbContext
+    {
+        public MyDBContext(DbContextOptions<MyDBContext> options) : base(options) { 
+        
+        }
+        public DbSet<Employee> Employees { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Employee>()
+                .Property(e => e.dept)
+                .HasConversion(new EnumToStringConverter<DEPARTMENT>());
+        }
+    }
+}
---
+++ employeenote/employeenote/Models/Employee.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace employeenote.Models
+{
+    public enum DEPARTMENT
+    {
+        HR,SALES,FININACE
+    }
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage ="Field is Empty")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Field is Empty")]
+        [EmailAddress]
+        public string email    { get; set; }
+        public string phone { get; set; }
+        public string city { get; set; }
+        [EnumDataType(typeof(DEPARTMENT))]
+        public DEPARTMENT dept { get; set; }
+        public Employee() {
+            Console.WriteLine("Emp");
+        }
+     }
+}
